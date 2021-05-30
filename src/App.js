f541ef4fe9945160d94b7412285e2c0b11e7b10c
Mithun Prabhu <prabhuofficial7@gmail.com>
2021-05-30 20:48:39 +0530

    app.js changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Fucking React
         </a>
       </header>
     </div>
