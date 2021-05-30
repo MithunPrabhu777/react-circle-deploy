5a544cd80af324d6244946d2494f52e70f6efc29
Mithun Prabhu <prabhuofficial7@gmail.com>
2021-05-30 21:23:50 +0530

    mithun changes added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Fucking React
+          Hello Mithun
         </a>
       </header>
     </div>
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+ 
